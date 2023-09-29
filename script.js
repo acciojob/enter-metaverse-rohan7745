33c0f4fe26cb5758b3f9367cad20744378a9f2a0
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-09-30 01:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// Get references to the HTML elements
+const statusParagraph = document.getElementById('status');
+const enterButton = document.getElementById('enterBtn');
+
+// Add a click event listener to the button
+enterButton.addEventListener('click', function() {
+    // Update the content of the 'status' paragraph
+    statusParagraph.textContent = "Entered Metaverse";
+});
+
