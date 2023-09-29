62745d82b4347dcad6eb811334bba0a23036d904
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-09-30 01:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-//your JS code here. If required.
-// Get references to the HTML elements
-const statusParagraph = document.getElementById('status');
-const enterButton = document.getElementById('enterBtn');
+// cypress/integration/tests/test.spec.js
 
-// Add a click event listener to the button
-enterButton.addEventListener('click', function() {
-    // Update the content of the 'status' paragraph
-    statusParagraph.textContent = "Entered Metaverse";
+describe('Test Metaverse Entry', () => {
+  it('Should change status text on button click', () => {
+    // Visit your web page
+    cy.visit('http://localhost:3000'); // Replace with your page's URL
+
+    // Click the "Enter" button
+    cy.get('#enterBtn').click();
+
+    // Wait for the text to change to "Entered Metaverse"
+    cy.get('#status').should('have.text', 'Entered Metaverse');
+  });
 });
-
